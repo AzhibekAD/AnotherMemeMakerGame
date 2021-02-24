cb59b85fdde0c94d1f5b7f7cb848f714f138a469
Azhibek Abdrakhmanov <azhik459@icloud.com>
2021-02-24 20:38:37 +0600

    Initial Commit

---
+++ AnotherMemeMakerGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnotherMemeMakerGame
+//
+//  Created by user on 24.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
